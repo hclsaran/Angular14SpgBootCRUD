1d4d6308217c6f97e82df275c54c95c4974330c6
hclsaran <renusaran@hotmail.com>
2022-08-23 10:56:20 -0400

    newly uploaded

---
+++ spring-boot-server/src/main/java/com/saran/SpringBootAngularApplication.java
@@ -0,0 +1,13 @@
+package com.saran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAngularApplication.class, args);
+	}
+
+}
